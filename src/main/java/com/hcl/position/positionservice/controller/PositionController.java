227f8f5ab07312edfe404c524c96bfdc16c41481
suranga <surangakumarage@gmail.com>
2024-04-27 14:00:27 +0530

    Add trade

--- src/main/java/com/hcl/position/positionservice/controller/PositionController.java
+++ src/main/java/com/hcl/position/positionservice/controller/PositionController.java
@@ -1,5 +1,6 @@
 package com.hcl.position.positionservice.controller;
 import com.hcl.position.positionservice.dto.PositionRequest;
+import com.hcl.position.positionservice.dto.TradeRequest;
 import com.hcl.position.positionservice.service.PositionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,12 +14,20 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class PositionController {
 
-    private final PositionService orderService;
+    private final PositionService positionService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addPosition(@RequestBody PositionRequest positionRequest) {
         log.info("Add Position");
-        orderService.addPosition(positionRequest);
+        positionService.addPosition(positionRequest);
+    }
+
+
+    @PostMapping("/trade")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void trade(@RequestBody TradeRequest tradeRequest) {
+        log.info("Trade");
+        positionService.trade(tradeRequest);
     }
 }
--- src/main/java/com/hcl/position/positionservice/service/PositionService.java
+++ src/main/java/com/hcl/position/positionservice/service/PositionService.java
@@ -1,8 +1,10 @@
 package com.hcl.position.positionservice.service;
 
 import com.hcl.position.positionservice.dto.PositionRequest;
+import com.hcl.position.positionservice.dto.TradeRequest;
 import com.hcl.position.positionservice.model.Position;
 import com.hcl.position.positionservice.repository.PositoinRepository;
+import com.hcl.position.positionservice.util.TRADE_TYPE;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class PositionService {
 
 
-    private final PositoinRepository  positoinRepository;
+    private final PositoinRepository positoinRepository;
 
 
     public void addPosition(PositionRequest positionRequest) {
@@ -23,6 +25,30 @@ public class PositionService {
                 .portfolio_id(positionRequest.getPortfolio_id())
                 .position_Name(positionRequest.getPosition_Name())
                 .units(positionRequest.getUnits()).build();
+
+        positoinRepository.save(position);
+    }
+
+    public void trade(TradeRequest tradeRequest) {
+
+
+        Position position = positoinRepository.getById(tradeRequest.getPosition_id());
+        if (null == position) {
+            log.info("invalid position");
+            return;
+        }
+        if ("BUY".equals(tradeRequest.getTradeType()))
+        {
+            position.setUnits(position.getUnits()+ tradeRequest.getUnits());
+        } else if ("SELL".equals(tradeRequest.getTradeType()) && position.getUnits() >= tradeRequest.getUnits())
+        {
+            position.setUnits(position.getUnits()-  tradeRequest.getUnits());
+
+        } else {
+            log.info("invalid Trade type - {}", tradeRequest.getTradeType());
+            return;
+
+        }
 
         positoinRepository.save(position);
     }
